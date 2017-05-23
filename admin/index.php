d28941abab52340695545ef0ae65eaa68d71fa3a
hex <ms0723595@gmail.com>
2017-05-23 22:12:27 +0800

    備份上傳

--- admin/index.php
+++ admin/index.php
@@ -7,10 +7,11 @@
 
 	$pages = array(
 		'main' 			=> '首頁',
-		'block' 		=> '黑名單' ,
-		'config' 		=> '座位設定 ',
-		'application' 	=> '申請紀錄' ,
+		'block' 		=> '黑名單',
+		'config' 		=> '座位詳細設定 ', //設定場地的細部設定 例如部分時段不開放
+		'application' 	=> '申請紀錄',
 		'notice'		=> '申請須知',
+		'seat'			=> '座位設定', //是拿來新增修改名稱跟停用啟用的
 		);
 
 	$page= isset($_GET['page'] )? $_GET['page']:'';
@@ -28,5 +29,37 @@
 
 	switch ($page) {
 		case 'main' :
-			$apply_query = $db -> prepare('SELECT COUNT(*) AS `c`FROM ``')
+			$apply_query = $db -> prepare('SELECT COUNT(*) AS `c`FROM `apply` WHERE `date` >=DATE(NOW()) AND `status` = 0 ') ;
+			$apply_query -> execute();
+			$apply_count = $apply_query->fetch();
+		
+			$seat_query = $db ->prepare('SELECT COUNT(*) AS `c` FROM' `seat`) ;
+			$seat_query -> execute();
+			$seat_count = $seat_query->fetch();
+
+			$seat_active_query = $db ->prepare('SELECT COUNT(*) AS `c` FROM `seat` WHERE `disabled` = 0 ');
+			$seat_active_query -> execute();
+			$seat_active_count = $seat_active_query->fetch();
+
+			$apply_month_query = $db ->prepare('SELECT COUNT(*) AS `c` FROM `apply` WHERE `date` >= :start AND `date` < :end');
+			$apply_month_query ->bindValue(':start', date('Y-m-d',mktime( 0, 0, 0, date('m') ,1 )));
+			$apply_month_query ->bindValue(':end' , date('y-m-d',mktime( 0, 0, 0, date('m') + 1 ,1)));
+			$apply_month_query ->execute();
+			$apply_month_count = $applt_month_query->fetch();
+
+			$apply_month_accepted_query = $db ->prepare('SELECT COUNT(*) AS `c` FROM `apply` WHERE `date` >=:start AND `date` < :end AND `status` = 1 ' );
+			$apply_month_accepted_query -> bindValue(':start', date('Y-m-d',mktime(0,0,0,date('m'),1)));
+			$apply_month_accepted_query -> bindValue(':end' ,date('Y-m-d',mktime(0,0,0,date('m')+1,1)));
+			$apply_month_accepted_query -> execute();
+			$apply_month_accepted_count = $apply_month_accepted_query->fetch();
+
+			$apply_history_query = $db ->prepare('SELECT COUNT(*) AS `c` FROM `apply`';
+			$apply_history_query ->execute();
+			$apply_history_count = $apply_history_query -> fetch();
+			
+			$apply_history_accepted_query = $db ->prepare('SELECT COUNT(*) AS `c` FROM `apply` WHERE `status` = 1 ');
+			$apply_history_accepted_query ->execute();
+			$apply_history_accepted_count = $apply_history_accepted_query->fetch();
+
+				
 	}
